2c0b259d07447ebed734ec9b52f9220d25d50446
Anisur Rahman <anisurlist3@gmail.com>
2024-10-18 12:32:00 +0600

    some keybinding added

--- lua/anis/core/keymaps.lua
+++ lua/anis/core/keymaps.lua
@@ -1,5 +1,3 @@
 vim.keymap.set("i", "jj", "<Esc>")
-
-
---plugins keymaps
---vim.keymap.set("n", "<leader>1", ":NvimTreeFindFileToggle<cr>")
+-- copy to clipboard
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y"]])
